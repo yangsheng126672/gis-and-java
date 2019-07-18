a2332a9a42e4680db81cd9ee9ff0d4c1e9d20084
liangrun@evercreative.com.cn <liangrun@evercreative.com.cn>
2019-07-18 20:51:56 +0800

    修改删除测量为逻辑删除

--- gis-service-beans/src/main/java/com/jdrx/gis/beans/entry/basic/MeasurementPO.java
+++ gis-service-beans/src/main/java/com/jdrx/gis/beans/entry/basic/MeasurementPO.java
@@ -1,7 +1,11 @@
 package com.jdrx.gis.beans.entry.basic;
 
-import java.math.BigDecimal;
+import lombok.Data;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
 public class MeasurementPO {
     /**
      * id
@@ -27,6 +31,31 @@ public class MeasurementPO {
      * 空间信息
      */
     private Object geom;
+
+    /**
+     * 是否删除
+     */
+    private Boolean deleteFlag;
+
+    /**
+     * 创建人
+     */
+    private String createBy;
+
+    /**
+     * 创建时间
+     */
+    private Date createAt;
+
+    /**
+     * 修改人
+     */
+    private String updateBy;
+
+    /**
+     * 修改时间
+     */
+    private Date updateAt;
 
     public Long getId() {
         return id;
@@ -66,5 +95,45 @@ public class MeasurementPO {
 
     public void setGeom(Object geom) {
         this.geom = geom;
+    }
+
+    public Boolean getDeleteFlag() {
+        return deleteFlag;
+    }
+
+    public void setDeleteFlag(Boolean deleteFlag) {
+        this.deleteFlag = deleteFlag;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy == null ? null : createBy.trim();
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy == null ? null : updateBy.trim();
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
     }
 }
