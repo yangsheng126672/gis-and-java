a3725fa655a3dc49a6ae6698f1302d3eb1ba72a5
luobin <luobin@evercreative.com.cn>
2019-12-24 11:22:15 +0800

    导出CAD修改(无法下载)

--- gis-service-beans/src/main/java/com/jdrx/gis/beans/dto/base/TypeIdDTO.java
+++ gis-service-beans/src/main/java/com/jdrx/gis/beans/dto/base/TypeIdDTO.java
@@ -1,5 +1,10 @@
 package com.jdrx.gis.beans.dto.base;
 
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.ToString;
+
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,16 +13,13 @@ import java.util.List;
  * @Author luobin
  * @Time 2019/12/6 0010 下午 3:57
  */
-
+@Data
+@ToString
 public class TypeIdDTO {
-    //id
+    @ApiModelProperty("设备类型ID")
+    @NotNull(message = "设备类型ID不能为空")
     Long id;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @ApiModelProperty("时间戳")
+    private String time;
 }
