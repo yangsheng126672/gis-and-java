d4888c5f7c5e4a5f077fe0fb70a048eb5b073bfb
liaosijun <liaosijun@evercreative.com.cn>
2020-01-13 17:38:18 +0800

    修复属性查询导出数据不准确

---
+++ gis-service-api/src/main/java/com/jdrx/gis/service/dataManage/VerifyDataService.java
@@ -0,0 +1,13 @@
+package com.jdrx.gis.service.dataManage;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 验证客户给到的设备数据
+ * @Author: liaosijun
+ * @Time: 2020/1/13 15:34
+ */
+@Service
+public class VerifyDataService {
+
+}
