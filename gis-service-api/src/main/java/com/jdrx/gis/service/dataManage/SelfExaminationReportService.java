fefe3c7bfa37ee6b5676b163cc3b746fb46961c9
liaosijun <liaosijun@evercreative.com.cn>
2020-01-15 13:26:16 +0800

    自检查报告 服务

---
+++ gis-service-api/src/main/java/com/jdrx/gis/service/dataManage/SelfExaminationReportService.java
@@ -0,0 +1,14 @@
+package com.jdrx.gis.service.dataManage;
+
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @Author: liaosijun
+ * @Time: 2020/1/14 10:13
+ */
+@Service
+public class SelfExaminationReportService {
+
+
+}
